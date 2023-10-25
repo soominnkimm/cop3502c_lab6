fe6b9cc7618ab817902bcca0e7667904515a772e
soomin kim <soomin.kim@ufl.edu>
2023-10-25 10:19:47 -0400

    commit message

--- main.py
+++ main.py
@@ -1,39 +1,28 @@
 #soomin kim
 def main():
-    print('1. encoder\n 2.decoder \n3. exit')
-    password_input = input('enter password')
-    menu_input = input('enter menu number')
+    print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+    menu_input = input('Please enter an option:')
+    password_input = input('Please enter your password to encode:')
     if menu_input == '1':
-        print(encoder(password_input))
+        encode(password_input)
+        print('Your password has been encoded and stored!')
     elif menu_input == '2':
-        print(decoder(password_input))
+        pass
     else:
         exit()
 
-def encoder(password):
+def encode(password):
     l = []
     for i in password:
-        l.append(i+3)
+        i += 3
+        if i >= 10:
+            i = int(str(i)[-1])
+            l.append(i)
+        else:
+            l.append(i)
+
     encoded = int(''.join(l))
     return encoded
-
-
-"""
-Written by: Case Zumbrum
-
-decoder decodes a password by decreasing all integers in it by 3
-
-Input:
-password (String): Encoded password, must be all integers and should be 8 characters long
-
-Output:
-decoded_password (String): Password decoded by reducing all integers by 3
-"""
-def decoder(password):
-    decoded_password = ''
-    for char in password:
-        decoded_password += str(int(char) - 3)
-    return decoded_password
 
 if __name__== '__main__':
     main()
